c608253f7478d169e26cf10f55c5d7bd227a5a4b
James Nacino <jamespnacino@gmail.com>
2023-01-19 13:54:33 -0500

    Update files
    Changes to app.py and requirements.txt

--- web_app/app.py
+++ web_app/app.py
@@ -2,7 +2,17 @@
 from myproject import app #import app from the __init__.py file
 from flask import render_template
 
+###############Commented out code for production only
+#from flask_limiter import Limiter
+#from flask_limiter.util import get_remote_address
 
+# set request limits to website. Limit 50,000 requests per day
+#limiter = Limiter(
+#    get_remote_address,
+#    app=app,
+#    default_limits=["50000 per day"],
+#    storage_uri="memcached://localhost:11211",
+#)
 
 @app.route('/')
 def INDEX():
@@ -10,4 +20,4 @@ def INDEX():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80) 
+    #app.run(host='0.0.0.0', port=80)
